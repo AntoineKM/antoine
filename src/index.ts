dc4c152a05839336b5d2f9b041b16e0c9e65d72c
Antoine Kingue <etorefr@gmail.com>
2023-04-25 10:58:49 +0200

    ✨ add eslint config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,44 @@
-export const foo = "bar";
+import { Linter } from "eslint";
+
+const config: Linter.Config = {
+  env: {
+    browser: true,
+    node: true,
+    es2022: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "universe/native"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2022,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  settings: {
+    "import/ignore": ["react-native"],
+  },
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+    "no-console": 0,
+    "no-control-regex": 0,
+    "import/extensions": [0],
+    "no-unused-vars": ["error", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false, "argsIgnorePattern": "^_" }],
+    "react/jsx-filename-extension": [1, { "extensions": [".tsx", ".ts"] }],
+    "react/jsx-props-no-spreading": [0],
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-indent-props": ["error", 2],
+    "react/jsx-curly-brace-presence": ["error", { "props": "always", "children": "always" }],
+    "prettier/prettier": ["error", { "endOfLine": "auto" }]
+  }
+}
+
+export default config;
